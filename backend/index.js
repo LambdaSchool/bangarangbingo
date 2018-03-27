f47b22e28f232700a2c9e790e1372d104454039c
Christian Franco <christian.franco94@icloud.com>
2018-03-27 01:31:29 -0700

    wip registiration

--- backend/index.js
+++ backend/index.js
@@ -1,18 +1,40 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const path = require('path');
+const User = require('./models/user');
 
 const server = express();
-const DB_URL = process.env.MONGODB_URI || 'mongodb://localhost/test';
+server.use(bodyParser.json());
+const DB_URL = process.env.MONGODB_URI || 'mongodb://localhost:27017/bingo';
 mongoose.connect(DB_URL);
-mongoose.Promise = global.Promise;
+mongoose.set('debug', true);
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
 server.use(express.static(path.join(__dirname, '../bingo/build')));
 
 server.post('/auth/register', async (req, res) => {
-  res.json({});
+  try {
+    const { username, password } = req.body;
+    if (!username || !password) {
+      res.json({
+        isValid: false,
+        errors: [
+          { message: 'Both username and password required' },
+        ],
+      });
+      return;
+    }
+    //check if user exists
+    
+    //create user
+    const user = await User.create({ username, password });
+    console.log(user);
+    res.json({});
+  } catch (e) {
+    console.log('something happened', e.name);
+  }
 });
 
 server.post('/auth/login', async (req, res) => {

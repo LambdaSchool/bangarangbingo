a8935eee632d2930853c20be15d15d01b4d36c9f
Cassidy SN Avery <tacolim@gmail.com>
2018-03-28 11:45:43 -0400

    billing & settings page routes

---
+++ bingo/src/components/setting.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Settings = () => (
+  <div>
+    <h1>Bangarang Bingo</h1>
+    <div className="settings">
+      USER SETTINGS PAGE
+    </div>
+    <Link to="/">LANDING PAGE</Link>
+  </div>
+);
+
+export default Settings;

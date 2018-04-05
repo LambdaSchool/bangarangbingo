d97b042b38c50a788dc042b47ca91a6592088539
Cassidy SN Avery <tacolim@gmail.com>
2018-04-05 14:53:28 -0400

    tidying up some code formatting

--- client/src/reducers/cardsReducer.js
+++ client/src/reducers/cardsReducer.js
@@ -2,7 +2,7 @@ import { GET_CARDS, ADD_CARD } from '../actions';
 
 export default (cards = [], action) => {
   if (action.type === GET_CARDS) {
-    console.log('CardsReducer action.payload', action.payload)
+    console.log('CardsReducer action.payload', action.payload);
   }
   switch (action.type) {
     case GET_CARDS:

5d12c8c06161856ccd3d3811c7a64912ed53b253
Cassidy SN Avery <tacolim@gmail.com>
2018-04-12 13:45:19 -0400

    updated landing page

--- client/src/components/landing.js
+++ client/src/components/landing.js
@@ -1,28 +1,26 @@
+/* eslint-disable */
+
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './landing.css';
 
 const Landing = () => (
   <div className="landing">
-    <div className="landing__content">
-      <div className="header"><img src="/images/logo-teal.gif" alt="Bangarang Bingo" /></div>
-      <div className="carousel">
-        <div className="icon-cards">
-          <div className="icon-cards__content">
-            <div className="icon-cards__item">BINGO</div>
-            <div className="icon-cards__item">BINGO</div>
-            <div className="icon-cards__item">BINGO</div>
-          </div>
-        </div>
-      </div>
-      <div className="text">
-        <p>Making your own bingo cards is time consuming and difficult.</p>
-        <p>Finally, an easy way to make beautiful,
-          print ready bingo cards for your parties and events.
+    <div className="background">
+      <video autoPlay muted loop className="background__video">
+        <source src="/video/people_talking.mp4" type="video/mp4" />
+      </video>
+    </div>
+    <div className="content">
+      <div className="content__header"><img src="/images/logo-teal.gif" alt="Bangarang Bingo" /></div>
+      <div className="content__textbox">
+        <p>Inject fun into your next gathering, party, or event with custom bingo cards.</p>
+        <p>
+          Making bingo cards can be time consuming and difficult, but now there’s an easy way to make beautiful,
+          print ready bingo cards. Simply input your words, choose some styling options, and we do the rest!
         </p>
-        <p>Simply input your words, choose some styling options, and we do the rest!</p>
       </div>
-      <div><Link to="/login" className="links__button">Make Your Bingo Card Now!</Link></div>
+      <div className="content__button"><Link to="/login" className="links__button">Make Your Bingo Card Now!</Link></div>
     </div>
   </div>
 );
